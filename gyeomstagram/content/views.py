d33cf7962d922c759ef4bd90c9e313d7e6fe58a4
HangyeomLee <h38lee@uwaterloo.ca>
2024-02-27 17:51:00 +0900

    Sending Datas from DB to HTML with using Views.py

--- gyeomstagram/content/views.py
+++ gyeomstagram/content/views.py
@@ -5,10 +5,5 @@ from .models import Feed
 # Create your views here.
 class Main_Content(APIView):
     def get(self, request):
-        feed_list = Feed.objects.all() # 쿼리 셋임
-
-        print(feed_list)
-
-
-        return render(request,"gyeomstagram\main.html")
-    
+        feed_list = Feed.objects.all().order_by('-id') # 쿼리 셋임
+        return render(request,"gyeomstagram\main.html", context = dict(feeds = feed_list))
