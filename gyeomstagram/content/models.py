ed397d1999a05a4395e481eebde403c4d50a4345
HangyeomLee <h38lee@uwaterloo.ca>
2024-02-27 10:55:38 +0900

    Done making MVP w/ HTML and CSS

--- gyeomstagram/content/models.py
+++ gyeomstagram/content/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Feed(models.Model):
+    content = models.TextField() # 글내용 
+    image = models.TextField() # 피드 이미지
+    profile_image = models.TextField() # 프로필 사진
+    user_id = models.TextField() # 글쓴이 아이디
+    like_count = models.TextField() # 좋아요 수(사실 안씀)
