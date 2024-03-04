55e3f4158dc84255389c9820535892ebbacb261d
HangyeomLee <h38lee@uwaterloo.ca>
2024-03-04 15:02:12 +0900

    Implement Customized User DB

--- gyeomstagram/gyeomstagram/settings.py
+++ gyeomstagram/gyeomstagram/settings.py
@@ -134,3 +134,6 @@ MEDIA_URL = '/media/' # 항상 / 로 끝나도록 설정
 
 # 업로드된 파일을 저장할 디렉토리 경로
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Customized User Model utilize
+AUTH_USER_MODEL = 'user.User'
--- gyeomstagram/user/models.py
+++ gyeomstagram/user/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
 
 # Create your models here.
+class User(AbstractBaseUser):
+    # 유저 프로필 사진이랑 아이디(닉네임), 유저 이멜주소 비밀번호도 있어햐함 
+    # 유저 이름도 있어야 함 실제 사용자 이름으로다가
+
+    profile_image = models.TextField()
+    nickname = models.CharField(max_length=24,unique = True)
+    name = models.CharField(max_length=24)
+    email = models.CharField(max_length = 50)
+
+    USERNAME_FIELD = 'nickname'
+
+
+    class Meta:
+        db_table = "User"
